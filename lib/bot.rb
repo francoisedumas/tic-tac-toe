1b4774ccaa99d2e558970a0a608ecb8b6ac9f227
Francois Dumas <dumaslattaque.francois@gmail.com>
2023-05-16 20:12:05 +0200

    Feat - Define a medium level bot

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,6 +1,77 @@
 class Bot
+  BASE = [-3, -1, 1, 3].freeze
+
+  WINNING_INDEXES = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ].freeze
+
+  attr_reader :level
+
+  def initialize(level = "easy")
+    @level = level
+  end
+
   def computer_position(board)
+    case @level
+    when "easy"
+      easy(board)
+    when "medium"
+      medium(board)
+    end
+  end
+
+  private
+
+  def easy(board)
     available_position = board.grep Integer
     available_position.sample
+  end
+
+  def medium(board)
+    # Get available indexes:
+    available_indexes = (board.grep Integer).map { |position| position - 1 }
+
+    # Get challenger positions example:
+    # ["X", 2, 3, 4, 5, 6, 7, 8, 9] gives [0]
+    challenger_indexes = []
+    board.each_with_index { |position, index| challenger_indexes << index if position == "X" }
+
+    # Check if challenger is about to win
+    indexes_to_play = critical_position(available_indexes, challenger_indexes)
+    # if yes, take the position to avoid challenger to win
+    return indexes_to_play.first + 1 if indexes_to_play.size == 1
+
+    # else for each challenger position, get the position to play
+    indexes_to_play = available_indexes_to_play(available_indexes, challenger_indexes)
+
+    # from available positions get one
+    indexes_to_play.sample + 1
+  end
+
+  def critical_position(available_indexes, challenger_indexes)
+    indexes_to_play = []
+    WINNING_INDEXES.each do |winning_indexes|
+      remaining_indexes = winning_indexes - (winning_indexes & challenger_indexes)
+      next if remaining_indexes.size != 1
+
+      indexes_to_play = remaining_indexes & available_indexes
+      break if indexes_to_play.size == 1
+    end
+    indexes_to_play
+  end
+
+  def available_indexes_to_play(available_indexes, challenger_indexes)
+    indexes_to_play = []
+    challenger_indexes.each do |index|
+      BASE.map { |base| indexes_to_play << index + base if index + base >= 0 && index + base <= 8 }
+    end
+    # keep only positions to play that are available
+    # comment position available to play can be repeated in the array it's not a problem
+    indexes_to_play &= available_indexes
   end
 end
--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -8,5 +8,38 @@ RSpec.describe Bot do
       board = ["X", "X", 3, "Y", "Y", 6, 7, 8, 9]
       expect([3, 6, 7, 8, 9].include?(bot.computer_position(board))).to be(true)
     end
+
+    context "when passed medium level" do
+      subject(:bot) { described_class.new("medium") }
+
+      it "returns 2 ou 4" do
+        board = ["X", 2, 3, 4, 5, 6, 7, 8, 9]
+        expect([2, 4].include?(bot.computer_position(board))).to be(true)
+      end
+
+      it "returns 2 ou 4 ou 6 ou 8" do
+        board = [1, 2, 3, 4, "X", 6, 7, 8, 9]
+        expect([2, 4, 6, 8].include?(bot.computer_position(board))).to be(true)
+      end
+
+      it "returns 3" do
+        board = ["X", "X", 3, "Y", 5, 6, 7, 8, 9]
+        expect(bot.computer_position(board)).to eq(3)
+      end
+    end
+  end
+
+  describe "#level" do
+    it "returns by default an easy bot level" do
+      expect(bot.level).to eq("easy")
+    end
+
+    context "when passed medium level" do
+      subject(:bot) { described_class.new("medium") }
+
+      it "returns a medium bot level" do
+        expect(bot.level).to eq("medium")
+      end
+    end
   end
 end
